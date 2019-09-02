6df9df9950f31c56faf875cf75fe0824d483d10f
wildone <max.barrass@gmail.com>
2019-09-02 17:24:05 +1000

    add error trap add retry for curl commands

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 function help() {
 echo "Usage:"
@@ -20,7 +21,12 @@ function download() {
 
     local FILENAME=$(basename "$FILEURL")
 
-    curl -L "${FILEURL}" -o ${FILENAME_PREFIX}${FILENAME}
+    curl --connect-timeout 5 \
+    --max-time 10 \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    -L "${FILEURL}" -o ${FILENAME_PREFIX}${FILENAME}
 
 }
 
@@ -33,7 +39,12 @@ function downloadAuth() {
 
     local FILENAME=$(basename "$FILEURL")
 
-    curl -u "${BASICCREDS}" -L "${FILEURL}" -o ${FILENAME_PREFIX}${FILENAME}
+    curl --connect-timeout 5 \
+    --max-time 10 \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    -v -u "${BASICCREDS}" -L "${FILEURL}" -o ${FILENAME_PREFIX}${FILENAME}
 
 }
 
