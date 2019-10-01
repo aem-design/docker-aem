07997333d1f6939f6e31ef77764a739cb2795d9e
wildone <max.barrass@gmail.com>
2019-10-01 23:47:14 +1000

    force retry for 403 abort if assets is not in metadata

--- scripts/githublatest.py
+++ scripts/githublatest.py
@@ -12,7 +12,7 @@ from requests.packages.urllib3.util.retry import Retry
 def requests_retry_session(
         retries=3,
         backoff_factor=0.3,
-        status_forcelist=(500, 502, 504),
+        status_forcelist=(403, 500, 502, 504),
         session=None,
 ):
     session = session or requests.Session()
@@ -28,31 +28,42 @@ def requests_retry_session(
     session.mount('https://', adapter)
     return session
 
+def main():
 
+    # set parameters for script
+    parser = argparse.ArgumentParser(description='Get download url from github releases with filename filter')
+    parser.add_argument('filter', help='file name filer')
+    parser.add_argument('url', help='url of github api')
+    # read command line params
+    args = parser.parse_args()
 
-# set parameters for script
-parser = argparse.ArgumentParser(description='Get download url from github releases with filename filter')
-parser.add_argument('filter', help='file name filer')
-parser.add_argument('url', help='url of github api')
-# read command line params
-args = parser.parse_args()
+    # get url content with retry
+    t0 = time.time()
+    try:
+        response = requests_retry_session().get(args.url)
+    except Exception as x:
+        print('Download failed', x.__class__.__name__, file=sys.stderr)
+        print(response, file=sys.stderr)
+        return
+    else:
+        print('Download succeeded', response.status_code, file=sys.stderr)
+    finally:
+        t1 = time.time()
+        print('Took', t1 - t0, 'seconds', file=sys.stderr)
 
-# get url content with retry
-t0 = time.time()
-try:
-    response = requests_retry_session().get(args.url)
-except Exception as x:
-    print('Download failed', x.__class__.__name__, file=sys.stderr)
-else:
-    print('Download succeeded', response.status_code, file=sys.stderr)
-finally:
-    t1 = time.time()
-    print('Took', t1 - t0, 'seconds', file=sys.stderr)
+    parsed_json = response.json()
 
-parsed_json = response.json()
+    if 'assets' not in parsed_json:
+        print('Error occurred getting: ' + args.url)
+        print(parsed_json)
+        return
 
-# find assets.browser_download_url that matches filter
-for asset in parsed_json['assets']:
-    if asset['browser_download_url'].find(args.filter) > 0:
-        print(asset['browser_download_url'])
+    # find assets.browser_download_url that matches filter
+    for asset in parsed_json['assets']:
+        if asset['browser_download_url'].find(args.filter) > 0:
+            print(asset['browser_download_url'])
 
+    return
+
+if __name__ == '__main__':
+    main()
